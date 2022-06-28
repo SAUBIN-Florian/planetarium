1148836e7af819fed161288a03ded30b693ad63c
floriansaubin <saubinflorian@gmail.com>
2022-06-28 12:00:34 +0200

    planets section almost done

--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -12,6 +12,7 @@ import Uranus from '../assets/planets/7_Uranus.png';
 import Neptune from '../assets/planets/8_Neptune.png';
 
 const DB = {
+    "0": "...",
     "1": "Mercury, the smallest planet in our solar system and closest to the Sun is only slightly larger than Earth's Moon. Mercury is the fastest planet, zipping around the Sun every 88 Earth days.",
     "2": "Venus spins slowly in the opposite direction from most planets. A thick atmosphere traps heat in a runaway greenhouse effect, making it the hottest planet in our solar system.",
     "3": "Earth, our home planet is the only place we know of so far that’s inhabited by living things. It's also the only planet in our solar system with liquid water on the surface.",
@@ -24,7 +25,7 @@ const DB = {
 
 export default function Planets() {
 
-  let [data, setData] = useState("1");
+  let [data, setData] = useState("0");
 
   const handleData = (e) => {
     setData(e.target.dataset.index);
@@ -39,37 +40,35 @@ export default function Planets() {
       </div>
       <div className="planets-imgs">
         <div className="planets-wrapper">
-          <img src={Mercure} id="mercury" data-index="1" onClick={handleData} alt="Mercury" />
-          <img src={Venus} id="venus" data-index="2" onClick={handleData} alt="Venus" />
-          <img src={Terre} id="earth" data-index="3" onClick={handleData} alt="Earth" />
-          <img src={Mars} id="mars" data-index="4" onClick={handleData} alt="Mars" />
-          <img src={Jupiter} id="jupiter" data-index="5" onClick={handleData} alt="Jupiter" />
-          <img src={Saturne} id="saturn" data-index="6" onClick={handleData} alt="Saturn" />
-          <img src={Uranus} id="uranus" data-index="7" onClick={handleData} alt="Uranus" />
-          <img src={Neptune} id="neptune" data-index="8" onClick={handleData} alt="Neptune" />
+          {/* hidden */}
+          <input type="radio" className="hidden-radio" name="radio-btn" id="radio1" />
+          <input type="radio" className="hidden-radio" name="radio-btn" id="radio2" />
+          <input type="radio" className="hidden-radio" name="radio-btn" id="radio3" />
+          <input type="radio" className="hidden-radio" name="radio-btn" id="radio4" />
+          <input type="radio" className="hidden-radio" name="radio-btn" id="radio5" />
+          <input type="radio" className="hidden-radio" name="radio-btn" id="radio6" />
+          <input type="radio" className="hidden-radio" name="radio-btn" id="radio7" />
+          <input type="radio" className="hidden-radio" name="radio-btn" id="radio8" />
+          {/* hidden */}
+          <img src={Mercure} id="mercury" data-index="1" alt="Mercury" />
+          <img src={Venus} id="venus" data-index="2" alt="Venus" />
+          <img src={Terre} id="earth" data-index="3" alt="Earth" />
+          <img src={Mars} id="mars" data-index="4" alt="Mars" />
+          <img src={Jupiter} id="jupiter" data-index="5" alt="Jupiter" />
+          <img src={Saturne} id="saturn" data-index="6" alt="Saturn" />
+          <img src={Uranus} id="uranus" data-index="7" alt="Uranus" />
+          <img src={Neptune} id="neptune" data-index="8" alt="Neptune" />
         </div>
       </div>
-      <div className="hidden-radio-inputs">
-        {/* hidden */}
-        <input type="radio" name="radio-btn" id="radio1" />
-        <input type="radio" name="radio-btn" id="radio2" />
-        <input type="radio" name="radio-btn" id="radio3" />
-        <input type="radio" name="radio-btn" id="radio4" />
-        <input type="radio" name="radio-btn" id="radio5" />
-        <input type="radio" name="radio-btn" id="radio6" />
-        <input type="radio" name="radio-btn" id="radio7" />
-        <input type="radio" name="radio-btn" id="radio8" />
-        {/* hidden */}
-      </div>
       <div className="planets-controls">
-        <label htmlFor="radio1" data-index="1" className="clickable-controls" onClick={handleData}></label>
-        <label htmlFor="radio2" data-index="2" className="clickable-controls" onClick={handleData}></label>
-        <label htmlFor="radio3" data-index="3" className="clickable-controls" onClick={handleData}></label>
-        <label htmlFor="radio4" data-index="4" className="clickable-controls" onClick={handleData}></label>
-        <label htmlFor="radio5" data-index="5" className="clickable-controls" onClick={handleData}></label>
-        <label htmlFor="radio6" data-index="6" className="clickable-controls" onClick={handleData}></label>
-        <label htmlFor="radio7" data-index="7" className="clickable-controls" onClick={handleData}></label>
-        <label htmlFor="radio8" data-index="8" className="clickable-controls" onClick={handleData}></label>
+        <label htmlFor="radio1" data-index="1" className="clickable-controls" data-name="MERCURY" onClick={handleData}></label>
+        <label htmlFor="radio2" data-index="2" className="clickable-controls" data-name="VENUS" onClick={handleData}></label>
+        <label htmlFor="radio3" data-index="3" className="clickable-controls" data-name="EARTH" onClick={handleData}></label>
+        <label htmlFor="radio4" data-index="4" className="clickable-controls" data-name="MARS" onClick={handleData}></label>
+        <label htmlFor="radio5" data-index="5" className="clickable-controls" data-name="JUPITER" onClick={handleData}></label>
+        <label htmlFor="radio6" data-index="6" className="clickable-controls" data-name="SATURN" onClick={handleData}></label>
+        <label htmlFor="radio7" data-index="7" className="clickable-controls" data-name="URANUS" onClick={handleData}></label>
+        <label htmlFor="radio8" data-index="8" className="clickable-controls" data-name="NEPTUNE" onClick={handleData}></label>
       </div>
     </div>
   )
